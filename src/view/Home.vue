1204894c09dfeeef83b9cf0a8c0a2e5402dc46e4
Songhay <soksonghay76@gmail.com>
2024-06-07 14:07:30 +0700

    last push

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,7 +2,7 @@
   <div class="hero_area">
     <div class="hero_bg_box">
       <div class="bg_img_box">
-        <img src= "/CADT/VueJs/finexo-vue/public/images/hero-bg.png" alt="" />
+        <img src= "/images/hero-bg.png" alt="" />
       </div>
     </div>
     <hero />
